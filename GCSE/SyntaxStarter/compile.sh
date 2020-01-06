2d95c3016a711ed0a4c598d49e4c8634d3bb562e
kj <github@kvickers.uk>
2020-01-06 21:21:06 +0000

    SyntaxStarter index file added.

--- GCSE/SyntaxStarter/compile.sh
+++ GCSE/SyntaxStarter/compile.sh
@@ -2,4 +2,6 @@
 
 pandoc -t revealjs SyntaxStarter.md -o SyntaxStarter.html -sV revealjs-url=https://revealjs.com
 
+cp SyntaxStarter.html index.html
+
 pandoc -t beamer SyntaxStarter.md -o SyntaxStarter.pdf
