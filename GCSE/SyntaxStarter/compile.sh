0b3c59b9ee633af230020759d4925fbe60e09e34
kj <github@kvickers.uk>
2020-01-07 11:26:06 +0000

    Changed the names within folders, as this causes duplication.

--- GCSE/SyntaxStarter/compile.sh
+++ GCSE/SyntaxStarter/compile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pandoc -t revealjs SyntaxStarter.md -o SyntaxStarter.html -sV revealjs-url=https://revealjs.com
+pandoc -t revealjs slides.md -o index.html -sV revealjs-url=https://revealjs.com
 
-cp SyntaxStarter.html index.html
-
-pandoc -t beamer SyntaxStarter.md -o SyntaxStarter.pdf
+pandoc -t beamer slides.md -o presentation.pdf
